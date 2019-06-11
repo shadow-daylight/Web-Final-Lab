439aeb1ec378d216b8ee9e55d534cf35194cdd52
shadow-daylight <candy40233@gmail.com>
2019-06-11 20:14:36 +0800

    rename files

--- index.js
+++
@@ -1,6 +0,0 @@
-window.onscroll = function() {scrollFunction()};
-
-function topFunction() {
-  document.body.scrollTop = 0; /*for safari*/
-  document.documentElement.scrollTop = 0;/*for other browser*/
-}
